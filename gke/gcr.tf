2a3fa3acfe60a1dd050af2293811bdd74004c5f3
Joakim Bomelin <joakim@bomelin.com>
2023-05-03 10:21:08 +0200

    Add GCR access to Kubernetes SA

--- gke/gcr.tf
+++ gke/gcr.tf
@@ -1,3 +1,9 @@
 resource "google_container_registry" "primary" {
   location = "EU"
 }
+
+resource "google_storage_bucket_iam_member" "viewer" {
+  bucket = google_container_registry.primary.id
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.kubernetes.email}"
+}
